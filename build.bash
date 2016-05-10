959603746070a568181ec1d97e37209ff9dd8863
Tony Gaetani <tony.gaetani@gmail.com>
2016-05-10 21:01:21 +0200

    Initial commit

---
+++ build.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IFS=
+# http://stackoverflow.com/a/246128/6554
+SOURCE="${BASH_SOURCE[0]}"
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do 
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$(readlink "$SOURCE")"
+	# if $SOURCE was a relative symlink, we need to resolve it relative to the
+	# path where the symlink file was located
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+IFS=$'\n\t'
+
+FILENAME="resume"
+if [[ -f "${FILENAME}.pdf" ]]; then
+    rm "${FILENAME}.pdf"
+fi
+
+pdflatex "${FILENAME}.tex"
+
